5eaa9cce064addeae02d8742ca346f3c77e3307f
Michael Herman <hermanmu@gmail.com>
2015-12-10 16:12:26 -0700

    updated to latest versions of python and django

--- .env/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/michaelherman/repos/realpython/django-slow-tests/.env/bin/python3.5
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
