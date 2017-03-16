acdbde5d86d425811086562176f6106caab20ba9
Michael Herman <hermanmu@gmail.com>
2017-03-16 09:35:55 -0600

    updated to python3.6, django 1.10, coverage 4.0, and added filter threshold option

---
+++ _examples/django110/polls/urls.py
@@ -0,0 +1,11 @@
+from django.conf.urls import url
+
+from . import views
+
+app_name = 'polls'
+urlpatterns = [
+    url(r'^$', views.IndexView.as_view(), name='index'),
+    url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
+    url(r'^(?P<pk>[0-9]+)/results/$', views.ResultsView.as_view(), name='results'),
+    url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
+]
---
+++ _examples/django110/venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/michael.herman/repos/realpython/django-slow-tests/_examples/django110/venv/bin/python3.6
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
