91202a636511bf7e6384f90105e512f3f1aa98d7
Peter Marsh <pete.d.marsh@gmail.com>
2017-06-16 10:50:52 +0100

    Fixed #16 - Incorrect test duration with freezegun
    freezegun is a Python library used to set the date/time returned by
    the Python date/time functions to make testing time-sensitive code
    easier.
    
    time.time() is used here to measure the duration of test cases, if
    freezegun was in use then the time returned by this could be the
    time the test had set up, rather than the actual time. This lead to
    test durations being reported as negative (if frozen time is in the
    past), or much longer than expected (if fronzen time is in the
    future).
    
    freezegun exposes the unmodified time.time() via an alias. This
    changes TimingSuite so that it uses this alias if freezegun is
    installed.

--- django_slowtests/tests/fake.py
+++ django_slowtests/tests/fake.py
@@ -1,5 +1,6 @@
 import time
 from django.test import TestCase
+from freezegun import freeze_time
 
 
 class FakeTestCase(TestCase):
@@ -15,3 +16,17 @@ class FakeTestCase(TestCase):
 
     def test_setup_class_was_run(self):
         self.assertTrue(self._setupClassRan)
+
+
+@freeze_time('2016-02-03 12:34:56')
+class FakeFrozenInPastTestCase(TestCase):
+
+    def test_this_should_not_have_a_negative_duration(self):
+        self.assertTrue(True)
+
+
+@freeze_time('3017-02-03 12:34:56')
+class FakeFrozenInFutureTestCase(TestCase):
+
+    def test_this_should_not_have_very_long_duration(self):
+        self.assertTrue(True)
