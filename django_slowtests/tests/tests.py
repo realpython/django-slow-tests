8dce6d5c13baa1c9a0e463ac829a9dac664ad9f3
Hugo Defrance <defrance.hugo@gmail.com>
2019-01-31 14:41:24 +0100

    Added parallel support

--- django_slowtests/tests/tests.py
+++ django_slowtests/tests/tests.py
@@ -1,13 +1,10 @@
 from django.test import TestCase
+from mock import patch
 from unittest import TestResult
-from ..testrunner import TimingSuite, TIMINGS
+from ..testrunner import TimingSuite
 
 
 class TimingSuiteTests(TestCase):
-
-    def setUp(self):
-        TIMINGS.clear()
-
     def test_add_a_test(self):
         from .fake import FakeTestCase
         suite = TimingSuite()
@@ -23,17 +20,22 @@ class TimingSuiteTests(TestCase):
         suite = TimingSuite()
         result = TestResult()
         suite.addTest(FakeFrozenInPastTestCase('test_this_should_not_have_a_negative_duration'))
-        suite.run(result)
+        with patch.object(suite, 'save_test_time') as mock:
+            suite.run(result)
         test_name = str(suite._tests[0])
-        self.assertTrue(TIMINGS[test_name] > 0)
-        self.assertTrue(TIMINGS[test_name] < 1)
+        mock.assert_called_once()
+        self.assertEqual(mock.call_args_list[0][0][0], test_name)
+        self.assertTrue(mock.call_args_list[0][0][1] > 0)
+        self.assertTrue(mock.call_args_list[0][0][1] < 1)
 
     def test_timing_is_correct_when_freezegun_sets_time_in_future(self):
         from .fake import FakeFrozenInFutureTestCase
         suite = TimingSuite()
         result = TestResult()
         suite.addTest(FakeFrozenInFutureTestCase('test_this_should_not_have_very_long_duration'))
-        suite.run(result)
+        with patch.object(suite, 'save_test_time') as mock:
+            suite.run(result)
         test_name = str(suite._tests[0])
-        self.assertTrue(TIMINGS[test_name] > 0)
-        self.assertTrue(TIMINGS[test_name] < 1)
+        self.assertEqual(mock.call_args_list[0][0][0], test_name)
+        self.assertTrue(mock.call_args_list[0][0][1] > 0)
+        self.assertTrue(mock.call_args_list[0][0][1] < 1)
