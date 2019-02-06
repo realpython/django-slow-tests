8469b47c502f383623f164dbcc626dc5511c052b
Hugo Defrance <defrance.hugo@gmail.com>
2019-02-06 14:23:41 +0100

    Fixed tests

--- django_slowtests/tests/tests.py
+++ django_slowtests/tests/tests.py
@@ -15,12 +15,15 @@ class TimingSuiteTests(TestCase):
             suite.run(result)
         self.assertEquals(len(suite._tests), 2)
         self.assertEquals(len(result.errors), 0)
+        self.assertEqual(mock.call_count, 2)
 
     def test_timing_is_correct_when_freezegun_sets_time_in_past(self):
         from .fake import FakeFrozenInPastTestCase
         suite = TimingSuite()
         result = TestResult()
-        suite.addTest(FakeFrozenInPastTestCase('test_this_should_not_have_a_negative_duration'))
+        suite.addTest(FakeFrozenInPastTestCase(
+            'test_this_should_not_have_a_negative_duration'
+        ))
         with patch.object(suite, 'save_test_time') as mock:
             suite.run(result)
         test_name = str(suite._tests[0])
@@ -33,7 +36,9 @@ class TimingSuiteTests(TestCase):
         from .fake import FakeFrozenInFutureTestCase
         suite = TimingSuite()
         result = TestResult()
-        suite.addTest(FakeFrozenInFutureTestCase('test_this_should_not_have_very_long_duration'))
+        suite.addTest(FakeFrozenInFutureTestCase(
+            'test_this_should_not_have_very_long_duration'
+        ))
         with patch.object(suite, 'save_test_time') as mock:
             suite.run(result)
         test_name = str(suite._tests[0])
